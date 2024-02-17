da443cee3e54d08a84107915bba343b9c6e72092
NhomNhom0 <phingoc.nhomnhom@gmail.com>
2024-02-17 21:15:33 +0700

    handle symbols in instruction

---
+++ elements/SymbolTable.py
@@ -0,0 +1,36 @@
+
+def SymbolsHandler(list):
+    # Predefined symbols
+    SYMBOLS = {
+        "SP": 0,
+        "LCL": 1,
+        "ARG": 2,
+        "THIS": 3,
+        "THAT": 4,
+        "SCREEN": 16384,
+        "KBD": 24576
+    }
+
+    # Add R0 to R15 to symbols
+    for i in range(16):
+        SYMBOLS[f'R{i}'] = i
+    
+    next_address = 16
+    address = 0
+# First pass: Process labels
+    for line in list:
+        if line.startswith('('):
+            symbol = line[1:-1]
+            SYMBOLS[symbol] = address
+        else:
+            address += 1
+
+    # Second pass: Process variables
+    for line in list:
+        if line.startswith('@'):
+            variable = line[1:]
+            if not variable.isdigit() and variable not in SYMBOLS:
+                SYMBOLS[variable] = next_address
+                next_address += 1
+    
+    return SYMBOLS
